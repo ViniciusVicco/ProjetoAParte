85ca3974a1372a12187e1bd64e98ff737eff5f70
vinicius.miranda <vinicius.miranda@tre-to.jus.br>
2019-10-23 18:01:39 -0300

    Projeto a parte

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nome/>
+
+
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import nome from './components/cabecalho.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    nome
+
   }
 }
 </script>
---
+++ src/components/cabecalho.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+       <header id="headerCabecalho">
+            <ul>
+                <li style="list-style-type:none">
+                    <p>Tabajara Motors</p>
+                    <button>Entrar</button>
+                    <button>Cadastre-se</button>
+                    <button>Catálogo</button>
+                    <button>Ajuda</button>
+                </li>
+            </ul>
+       </header>
+    </div>
+</template>
+
+<style>
+    #headerCabecalho{
+        position: absolute;
+        background-color: #1E95EA;
+        width: auto;
+        width: 100%;
+
+    }
+
+
+</style>
