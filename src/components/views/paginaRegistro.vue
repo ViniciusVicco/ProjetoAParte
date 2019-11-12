420bb8c9808facce883f13de2bfa856eb228590e
vinicius.miranda <vinicius.miranda@tre-to.jus.br>
2019-11-12 15:03:46 -0300

    Tela De cadastro com termos e e-mail ativado. Falta construir um formulário melhor.

--- src/components/views/paginaRegistro.vue
+++ src/components/views/paginaRegistro.vue
@@ -1,10 +1,58 @@
-<template>
-    
-</template>
+
+    <template>
+        <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                    <div class="card card-signin my-5">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Cadastrar-se</h5>
+                            <form class="form-signin">
+                                <div class="form-label-group">
+                                    <input type="email" id="inputEmail" class="form-control" placeholder="E-mail" required autofocus>
+                                </div>
+                                <div class="form-label-group">
+                                    <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+                                </div>
+                                <div class="custom-control custom-checkbox mb-3">
+                                    <input type="checkbox" class="custom-control-input" v-model="estadoF" id="customCheck1">
+                                    <label class="custom-control-label" for="customCheck1">Li e concordo com as políticas da empresa.</label>
+                                    </div>
+                                <div class="custom-control custom-checkbox mb-3">
+                                    <input type="checkbox" class="custom-control-input" v-model="estadoV" id="customCheck2">
+                                    <label class="custom-control-label" for="customCheck2">Desejo receber e-mails e promoções.</label>
+                                </div>
+                                <button v-if="estadoF" class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </body>
+
+    </template>
+
+    <style>
+        #headerCabecalho{
+            position: absolute;
+            background-color: #1E95EA;
+            width: auto;
+            width: 100%;
+
+        }
+    </style>
+
 
 <script>
     export default {
-        name: "Registrar"
+        name: "Registrar",
+        data(){
+            return {
+                estadoF: false,
+                estadoV: true
+            }
+        }
     }
 </script>
 
