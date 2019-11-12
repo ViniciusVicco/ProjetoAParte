f6c13389a75806894f539746deb2b3eccca01814
vinicius.miranda <vinicius.miranda@tre-to.jus.br>
2019-11-12 15:25:00 -0300

    Criando primeiros formulários

--- src/components/views/paginaRegistro.vue
+++ src/components/views/paginaRegistro.vue
@@ -11,9 +11,21 @@
                                 <div class="form-label-group">
                                     <input type="email" id="inputEmail" class="form-control" placeholder="E-mail" required autofocus>
                                 </div>
+                                <br>
+                                <div class="form-label-group">
+                                    <input type="email" id="inputCpf" class="form-control" placeholder="CPF/CNPJ" required autofocus>
+                                </div>
+                                <br>
+                                <div class="form-label-group">
+                                    <input type="email" id="inputTelefone" class="form-control" placeholder="Telefone" required autofocus>
+                                </div>
+                                <br>
                                 <div class="form-label-group">
                                     <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+                                    <br>
+                                    <input type="password" id="inputConfirmPassword" class="form-control" placeholder="Senha" required>
                                 </div>
+                                <br>
                                 <div class="custom-control custom-checkbox mb-3">
                                     <input type="checkbox" class="custom-control-input" v-model="estadoF" id="customCheck1">
                                     <label class="custom-control-label" for="customCheck1">Li e concordo com as políticas da empresa.</label>
@@ -23,6 +35,7 @@
                                     <label class="custom-control-label" for="customCheck2">Desejo receber e-mails e promoções.</label>
                                 </div>
                                 <button v-if="estadoF" class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+                                <button v-if="estadoV" class="btn btn-lg btn-primary btn-block" onclick="alert('Aceite as políticas da empresa para prosseguir.')" type="submit">Ajuda</button>
                             </form>
                         </div>
                     </div>
