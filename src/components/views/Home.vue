bb295d636eb700d70ee69f898772cb2d02876d20
vinicius.miranda <vinicius.miranda@tre-to.jus.br>
2019-11-13 15:49:53 -0300

    Layout das páginas finalizado.

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,5 +1,37 @@
 <template>
     <div>
+        <div class="card-deck">
+            <div class="card">
+                <img src="../../assets/images/hatches.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Melhores Hatches usados entre 2014 e 2015</h5>
+                    <p class="card-text">VW Golf apresenta uma crescente procura em seus modelos.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../assets/images/hatches.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../../assets/images/hatches.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
+        </div>
         <br>
         <div class="card-deck">
             <div class="card" style="width: 18rem; background-color: #CCCCCC">
