6fa99bd88092407c47027ec68a4cf40d1077acd8
vinicius.miranda <vinicius.miranda@tre-to.jus.br>
2019-11-13 15:46:21 -0300

    Layout das páginas finalizado.

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,33 +1,38 @@
 <template>
-    <div class="card-deck">
-        <div class="card">
-            <img src="../../assets/images/hatches.jpeg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <div>
+        <br>
+        <div class="card-deck">
+            <div class="card" style="width: 18rem; background-color: #CCCCCC">
+<!--                <img class="card-img-top" src="../../assets/images/finan.png.jpeg" alt="Card image cap">-->
+                <div class="card-body">
+                    <h5 class="card-title">Financiamento</h5>
+                    <p class="card-text">Financiamos seu veículo em até 60x, acesse nosso site parceiro.</p>
+                    <a href="https://www.icarros.com.br/financiamento/index.jsp?ano=2013&parceiro_id=192&midia_id=5583&gclid=CjwKCAiA8K7uBRBBEiwACOm4dzPPkRW6DzpmeJHXPHHmjL76tCR6pCK9lzGRIFj1kxgnCjl-myG8YxoCvBoQAvD_BwE" class="btn btn-primary">Entenda Mais</a>
+                </div>
             </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-        </div>
-        <div class="card">
-            <img src="../../assets/images/hatches.jpeg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
+            <div class="card" style="width: 18rem; background-color: #CCCCCC">
+                <div class="card-body">
+                    <h5 class="card-title">Acesse Nosso Catálogo</h5>
+                    <p class="card-text">Nosso catálogo possuí os mais variados tipos de veículos, confira.</p>
+                    <router-link to="/catalogo"><button class="btn btn-primary">Acessar Catálogo</button></router-link>
+                </div>
             </div>
         </div>
-        <div class="card">
-            <img src="../../assets/images/hatches.jpeg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <br>
+        <div class="card-deck">
+            <div class="card" style="width: 18rem; background-color: #CCCCCC">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
+            <div class="card" style="width: 18rem; background-color: #CCCCCC">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
         </div>
     </div>
--- src/components/views/paginaCatalogo.vue
+++ src/components/views/paginaCatalogo.vue
@@ -1,8 +1,55 @@
 <template>
-    <div id="catalogo">
-        <ul>
-
-        </ul>
+    <div class="card-deck">
+        <div class="card">
+            <img src="../../assets/images/hatche.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Hatch</h5>
+                <button class="btn btn-lg btn-primary btn-block">Filtrar Modelo</button>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Atualizado em 14/11/2019</small>
+            </div>
+        </div>
+        <div class="card">
+            <img src="../../assets/images/suv.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Suv</h5>
+                <button class="btn btn-lg btn-primary btn-block">Filtrar Modelo</button>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Atualizado em 14/11/2019</small>
+            </div>
+        </div>
+        <div class="card">
+            <img src="../../assets/images/sedans.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Sedan</h5>
+                <button class="btn btn-lg btn-primary btn-block">Filtrar Modelo</button>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Atualizado em 14/11/2019</small>
+            </div>
+        </div>
+        <div class="card">
+            <img src="../../assets/images/esportivos.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Esportivo</h5>
+                <button class="btn btn-lg btn-primary btn-block">Filtrar Modelo</button>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Atualizado em 14/11/2019</small>
+            </div>
+        </div>
+        <div class="card">
+            <img src="../../assets/images/importado.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Importação</h5>
+                <button class="btn btn-lg btn-primary btn-block">Filtrar Modelo</button>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Atualizado em 14/11/2019</small>
+            </div>
+        </div>
     </div>
 </template>
 
